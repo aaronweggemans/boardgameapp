06e97795e2c0dbc7d048f14cc68e32996d776ff8
aaronweggemans <aaronweggemans@hotmail.nl>
2018-10-30 13:52:11 +0100

    end

--- routes.php
+++ routes.php
@@ -3,45 +3,49 @@
  * GET routes
  */
 
+$url_prefix_empty='~s1130601/P1_OOAPP_Opdracht';
+$url_prefix=$url_prefix_empty.'/';
+
 //If session Login exists, your able to go the the next routes
 if($_SESSION['login'])
 {
+
     //GET
-    $router->get('home', 'controllers/index.php');
-    $router->get('players', 'controllers/players.php');
-    $router->get('games', 'controllers/games.php');
-    $router->get('users', 'controllers/users.php');
-    $router->get('battles', 'controllers/battles.php');
-    $router->get('excercise', 'controllers/excercise.php');
-    $router->get('logout', 'controllers/logout.php');
+    $router->get($url_prefix.'home', 'controllers/index.php');
+    $router->get($url_prefix.'players', 'controllers/players.php');
+    $router->get($url_prefix.'games', 'controllers/games.php');
+    $router->get($url_prefix.'users', 'controllers/users.php');
+    $router->get($url_prefix.'battles', 'controllers/battles.php');
+    $router->get($url_prefix.'excercise', 'controllers/excercise.php');
+    $router->get($url_prefix.'logout', 'controllers/logout.php');
 
     //VERWIJDERIE
-    $router->post('del_user', 'controllers/post/del_user.php');
-    $router->post('del_game', 'controllers/post/del_game.php');
-    $router->post('del_battle', 'controllers/post/del_battle.php');
+    $router->post($url_prefix.'del_user', 'controllers/post/del_user.php');
+    $router->post($url_prefix.'del_game', 'controllers/post/del_game.php');
+    $router->post($url_prefix.'del_battle', 'controllers/post/del_battle.php');
 
     //POST
-    $router->post('add_player', 'controllers/post/add_player.php');
-    $router->post('add_user', 'controllers/post/add_user.php');
-    $router->post('del_game', 'controllers/post/del_game.php');
-    $router->post('add_game', 'controllers/post/add_game.php');
-    $router->post('add_battle', 'controllers/post/add_battle.php');
+    $router->post($url_prefix.'add_player', 'controllers/post/add_player.php');
+    $router->post($url_prefix.'add_user', 'controllers/post/add_user.php');
+    $router->post($url_prefix.'del_game', 'controllers/post/del_game.php');
+    $router->post($url_prefix.'add_game', 'controllers/post/add_game.php');
+    $router->post($url_prefix.'add_battle', 'controllers/post/add_battle.php');
 
     //BUILD
-    $router->get('createUser', 'controllers/createUser.php');
-    $router->get('creategame', 'controllers/createGame.php');
-    $router->get('addBattle', 'controllers/createBattle.php');
+    $router->get($url_prefix.'createUser', 'controllers/createUser.php');
+    $router->get($url_prefix.'creategame', 'controllers/createGame.php');
+    $router->get($url_prefix.'addBattle', 'controllers/createBattle.php');
 
     //SENDING MAIL
-    $router->post('send_mail', 'controllers/post/send_mail.php');
+    $router->post($url_prefix.'send_mail', 'controllers/post/send_mail.php');
 }
 else {
     //else if the session not exists your able to route to this
 
     //GET
-    $router->get('', 'controllers/login.php');
-    $router->get('login', 'controllers/login.php');
+    $router->get($url_prefix_empty.'', 'controllers/login.php');
+    $router->get($url_prefix.'login', 'controllers/login.php');
 
     //POST
-    $router->post('login', 'controllers/post/login.php');
+    $router->post($url_prefix.'login', 'controllers/post/login.php');
 }
